b287ac7d29bb48807d608528f83dfb53771b3566
pyarchinit <pyarchinit@gmail.com>
2014-04-17 17:35:24 +0200

    Aggiornamento PDF TAfonomia in progress
    Aggiornamento exp pdf in progress

--- pyarchinit/modules/utility/pyarchinit_exp_Tafonomiasheet_pdf.py
+++ pyarchinit/modules/utility/pyarchinit_exp_Tafonomiasheet_pdf.py
@@ -101,7 +101,7 @@ class single_Tafonomia_pdf_sheet:
 
 		#0 row
 		intestazione = Paragraph("<b>SCHEDA TAFONOMICA<br/>" + str(self.datestrfdate()) + "</b>", styNormal)
-		intestazione2 = Paragraph("<b>pyArchInit</b><br/>www.pyarchinit.blogspot.com", styNormal)
+		intestazione2 = Paragraph("<b>pyArchInit</b><br/>pyarchinit", styNormal)
 
 		#1 row
 		sito = Paragraph("<b>Sito</b><br/>"  + str(self.sito), styNormal)
@@ -110,37 +110,60 @@ class single_Tafonomia_pdf_sheet:
 		nr_scheda = Paragraph("<b>Nr. Scheda</b><br/>"  + str(self.nr_scheda_taf), styNormal)
 
 		#2 row
-		rito = Paragraph("<b>Rito</b><br/>"  + self.rito, styNormal)
+		elementi_strutturali = Paragraph("<b>ELEMENTI STRUTTURALI</b></b>",styNormal)
+
+		#3 row
 		tipo_contenitore_resti = Paragraph("<b>Tipo contenitore resti</b><br/>"  + str(self.tipo_contenitore_resti), styNormal)
 		tipo_copertura = Paragraph("<b>Tipo copertura</b><br/>"  + str(self.copertura_tipo), styNormal)
+		segnacoli = Paragraph("<b>Segnacoli</b><br/>"  + str(self.segnacoli), styNormal)
+		canale_libatorio = Paragraph("<b>Canale libatorio</b><br/>"  + str(self.canale_libatorio_si_no), styNormal)
+
+		#4 row
+		dati_deposizionali = Paragraph("<b>DATI DEPOSIZIONALI INUMATO<b></b>",styNormal)
+
+		#5 row
+		rito = Paragraph("<b>Rito</b><br/>"  + self.rito, styNormal)
 		orientamento_asse = Paragraph("<b>Orientamento asse</b><br/>"  + str(self.orientamento_asse), styNormal)
 		if str(self.orientamento_azimut) == "None":
 			orientamento_azimut = Paragraph("<b>Azimut</b><br/>", styNormal)
 		else:
 			orientamento_azimut_conv = self.PU.conversione_numeri(self.orientamento_azimut)
 			orientamento_azimut = Paragraph("<b>Azimut</b><br/>"  + orientamento_azimut_conv + "°", styNormal)
+		posizione_cranio = Paragraph("<b>Posizione cranio</b><br/>"  + str(self.posizione_cranio), styNormal)
 
-		#3 row
-		segnacoli = Paragraph("<b>Segnacoli</b><br/>"  + str(self.segnacoli), styNormal)
-		canale_libatorio = Paragraph("<b>Canale libatorio</b><br/>"  + str(self.canale_libatorio_si_no), styNormal)
-		oggetti_rinv_esterno = Paragraph("<b>Oggetti rinvenuti all'esterno</b><br/>"  + str(self.oggetti_rinvenuti_esterno), styNormal)
-		stato_conservazione = Paragraph("<b>Stato di conservazione</b><br/>"  + str(self.stato_di_conservazione), styNormal)
-
-		#4 row
+		#6 row
+		posizione_scheletro = Paragraph("<b>Posizione scheletro</b><br/>"  + str(self.posizione_scheletro), styNormal)
 		if str(self.lunghezza_scheletro) == "None":
 			lunghezza_scheletro = Paragraph("<b>Lunghezza scheletro</b><br/>", styNormal)
 		else:
 			lunghezza_scheletro_conv = self.PU.conversione_numeri(self.lunghezza_scheletro)
-
-			lunghezza_scheletro = Paragraph("<b>Lunghezza scheletro</b><br/>"  + lunghezza_scheletro_conv + " m", styNormal)
-		posizione_scheletro = Paragraph("<b>Posizione scheletro</b><br/>"  + str(self.posizione_scheletro), styNormal)
-		posizione_cranio = Paragraph("<b>Posizione cranio</b><br/>"  + str(self.posizione_cranio), styNormal)
-
-		#5 row
+			lunghezza_scheletro = Paragraph("<b>Lunghezza scheletro</b><br/>"  + lunghezza_scheletro_conv +" m", styNormal)
 		posizione_arti_superiori = Paragraph("<b>Posizione arti superiori</b><br/>"  + str(self.posizione_arti_superiori), styNormal)
 		posizione_arti_inferiori = Paragraph("<b>Posizione arti inferiori</b><br/>"  + str(self.posizione_arti_inferiori), styNormal)
 
-		#6 row
+		#7 row
+		dati_postdeposizionali = Paragraph("<b>DATI POSTDEPOSIZIONALI<b></b>",styNormal)
+
+		#8 row
+		stato_conservazione = Paragraph("<b>Stato di conservazione</b><br/>"  + str(self.stato_di_conservazione), styNormal)
+		disturbato = Paragraph("<b>Disturbato</b><br/>"  + str(self.segnacoli), styNormal)
+		completo = Paragraph("<b>Completo</b><br/>"  + str(self.canale_libatorio_si_no), styNormal)
+		in_connessione = Paragraph("<b></b><br/>"  + str(self.oggetti_rinvenuti_esterno), styNormal)
+
+		#9 row
+		caratteristiche_tafonomiche = ''
+		caratteristiche_list = eval(self.caratteristiche)
+		if len(caratteristiche_list) > 0 :
+			for i in caratteristiche_list:
+				if caratteristiche_tafonomiche == '':
+					caratteristiche_tafonomiche = ("Tipo caratteristica: %s, posizione: %s") % (str(i[0]), str(i[1]))
+				else:
+					caratteristiche_tafonomiche += ("<br/>Tipo caratteristica: %s, posizione: %s") % (str(i[0]), str(i[1]))
+
+		caratteristiche_tafonomiche_txt = Paragraph("<b>CARATTERISTICHE TAFONOMICHE</b><br/>"  + caratteristiche_tafonomiche, styNormal)
+
+
+		#10 row
 		descrizione = ''
 		try:
 			descrizione = Paragraph("<b>Descrizione</b><br/>" + str(self.descrizione_taf), styDescrizione)
@@ -153,11 +176,13 @@ class single_Tafonomia_pdf_sheet:
 		except:
 			pass
 		
-		#7 row
-		corredo = Paragraph("<b>Corredo</b></b>",styNormal)
+		#11 row
+		corredo = Paragraph("<b>CORREDO</b></b>",styNormal)
+
+		#12 row
 		corredo_presente = Paragraph("<b>Corredo presente</b><br/>" + str(self.corredo_presenza),styDescrizione)
 
-		#8 row
+		#13 row
 		corredo_tipo = ''
 		if eval(self.corredo_tipo) > 0 :
 			for i in eval(self.corredo_tipo):
@@ -172,37 +197,24 @@ class single_Tafonomia_pdf_sheet:
 					except:
 						pass
 
-		corredo_tipo_txt = Paragraph("<b>Corredo tipo</b><br/>"  + corredo_tipo, styNormal)
-
-		#9 row
-		caratteristiche_tafonomiche = ''
-		if eval(self.caratteristiche) > 0 :
-			for i in eval(self.caratteristiche):
-				if caratteristiche_tafonomiche == '':
-					try:
-						caratteristiche_tafonomiche += ("Tipo caratteristica %s, posizione: %s") % (str(i[0]), str(i[1]), str(i[2]))
-					except:
-						pass
-				else:
-					try:
-						caratteristiche_tafonomiche += ("<br/>Tipo caratteristica %s, posizione: %s") % (str(i[0]), str(i[1]), str(i[2]))
-					except:
-						pass
-
-		caratteristiche_tafonomiche_txt = Paragraph("<b>Caratteristiche tafonomiche</b><br/>"  + caratteristiche_tafonomiche, styNormal)
+		corredo_tipo_txt = Paragraph("<b>Singoli oggetti di corredo</b><br/>"  + corredo_tipo, styNormal)
 
 		#schema
 		cell_schema =  [ #00, 01, 02, 03, 04, 05, 06, 07, 08, 09 rows
-						[intestazione, '01', '02', '03', '04','05', '06', intestazione2, '08', '09'], #0 row ok
-		 				[sito, '01', '02', '03', '04', sigla_struttura, '06', '07',nr_individuo,nr_scheda], #1 row ok
-						[rito, '01', '02',tipo_contenitore_resti,'04', tipo_copertura,'06', orientamento_asse, '08',orientamento_azimut], #2 row ok
-						[segnacoli, '01', '02',canale_libatorio,'04', oggetti_rinv_esterno,'06', stato_conservazione, '08','09'], #3 row ok
-						[lunghezza_scheletro, '01', '02',posizione_scheletro,'04', '05', posizione_cranio,'07', '08','09'], #4row ok
-						[posizione_arti_superiori, '01','02', '03', '04', posizione_arti_inferiori, '06', '07', '08', '09'], #5 row ok
-						[descrizione, '01','02', '03', '04', interpretazione, '06', '07', '08', '09'], #6 row ok
-						[corredo,'01', '02', '03', '04',corredo_presente, '06', '07', '08', '09'], #7 row
-						[corredo_tipo_txt, '01', '02', '03', '04', '05', '06', '07', '08', '09'], #8 row
-						[caratteristiche_tafonomiche_txt, '01', '02', '03', '04', '05', '06', '07', '08', '09'] #9 row
+						[intestazione, '01', '02', '03', '04','05', '06', intestazione2, '08', '09'], #0 row  ok
+						[sito, '01', '02', '03', '04', sigla_struttura, '06', '07',nr_individuo,nr_scheda], #1 row ok
+						[elementi_strutturali, '01', '02', '03', '04', '07', '06', '07','08', '09'], #2 row ok
+						[tipo_contenitore_resti, '01', '02', tipo_copertura,'04', segnacoli,'06', canale_libatorio, '08'], #3 row ok
+						[dati_deposizionali, '01', '02','03','04', '05','06', '07', '08','09'], #4 row ok
+						[rito, '01', '02',orientamento_asse,'04', orientamento_azimut,'06', 'posizione_cranio', '08','09'], #5 row ok
+						[posizione_scheletro, '01', lunghezza_scheletro, '03', posizione_arti_superiori,'05','06', posizione_arti_inferiori, '08','09'], #6 row ok
+						[dati_postdeposizionali, '01', '02','03','04', '05','06', '07', '08','09'], #7 row ok
+						[stato_conservazione, '01', '02', disturbato,'04', completo,'06', in_connessione, '08'], #8 row ok
+						[caratteristiche_tafonomiche_txt, '01', '02','03','04', '05','06', '07', '08','09'], #9 row ok
+						[descrizione, '01','02', '03', '04', interpretazione, '06', '07', '08', '09'], #10 row ok
+						[corredo, '01', '02', '03', '04', '05', '06', '07', '08', '09'], #11 row ok
+						[corredo_presente,'01', '02', '03', '04','05', '06', '07', '08', '09'], #12row
+						[corredo_tipo_txt,'01', '02', '03', '04','05', '06', '07', '08', '09'] #13 row
 						]
 
 		#table style
@@ -219,42 +231,55 @@ class single_Tafonomia_pdf_sheet:
 					('SPAN', (9,1),(9,1)),  #dati identificativi
 
 					#2 row
-					('SPAN', (0,2),(2,2)),  #Definizione - interpretazone
-					('SPAN', (3,2),(4,2)),  #definizione - intepretazione
-					('SPAN', (5,2),(6,2)),  #definizione - intepretazione
-					('SPAN', (7,2),(8,2)),  #definizione - intepretazione
-					('SPAN', (9,2),(9,2)),  #definizione - intepretazione
+					('SPAN', (0,2),(9,2)),  #Elementi strutturali
 
 					#3 row
-					('SPAN', (0,3),(2,3)),  #conservazione - consistenza - colore
-					('SPAN', (3,3),(4,3)),  #conservazione - consistenza - colore
-					('SPAN', (5,3),(6,3)),  #conservazione - consistenza - colore
-					('SPAN', (7,3),(9,3)),  #conservazione - consistenza - colore
+					('SPAN', (0,3),(2,3)),  #
+					('SPAN', (3,3),(4,3)),  #
+					('SPAN', (5,3),(6,3)),  #
+					('SPAN', (7,3),(9,3)),  #
 
 					#4 row
-					('SPAN', (0,4),(2,4)),  #inclusi - campioni - formazione
-					('SPAN', (3,4),(5,4)),  #inclusi - campioni - formazione
-					('SPAN', (6,4),(9,4)),  #inclusi - campioni - formazione
+					('SPAN', (0,4),(9,4)),  #
 
 					#5 row
-					('SPAN', (0,5),(4,5)),  #iniziale
-					('SPAN', (5,5),(9,5)),  #periodo inizlae
-					('VALIGN',(0,5),(0,5),'TOP'), 
-					('VALIGN',(5,5),(5,5),'TOP'), 
+					('SPAN', (0,5),(2,5)),  #
+					('SPAN', (3,5),(4,5)),  #
+					('SPAN', (5,5),(6,5)),  #
+					('SPAN', (7,5),(9,5)),  #
 
 					#6 row
-					('SPAN', (0,6),(4,6)),  #Attivita - Struttura - Quota min - Quota max
-					('SPAN', (5,6),(9,6)),  #Attivita - Struttura - Quota min - Quota max
+					('SPAN', (0,6),(1,6)),  #
+					('SPAN', (2,6),(3,6)),  #
+					('SPAN', (4,6),(6,6)),  #
+					('SPAN', (7,6),(9,6)),  #
 
 					#7 row
-					('SPAN', (0,7),(4,7)),  #Attivita - Struttura - Quota min - Quota max
-					('SPAN', (5,7),(9,7)),  #Attivita - Struttura - Quota min - Quota max
+					('SPAN', (0,7),(9,7)),  #
 
 					#8 row
-					('SPAN', (0,8),(9,8)),  #Attivita - Struttura - Quota min - Quota max
-					
+					('SPAN', (0,8),(2,8)),  #
+					('SPAN', (3,8),(4,8)),  #
+					('SPAN', (5,8),(6,8)),  #
+					('SPAN', (7,8),(9,8)),  #
+
 					#9 row
-					('SPAN', (0,9),(9,9)),  #Attivita - Struttura - Quota min - Quota max
+					('SPAN', (0,9),(9,9)),  #
+
+					#10 row
+					('SPAN', (0,10),(4,10)),  #
+					('SPAN', (5,10),(9,10)),  #
+
+					#11 row
+					('SPAN', (0,11),(9,11)),  #
+
+
+					#12 row
+					('SPAN', (0,12),(9,12)),  #
+
+
+					#13 row
+					('SPAN', (0,13),(9,13)),  #
 
 					('VALIGN',(0,0),(-1,-1),'TOP')
 
