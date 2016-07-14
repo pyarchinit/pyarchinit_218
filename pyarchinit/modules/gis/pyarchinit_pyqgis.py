2132b7082a8160c23c59224318686dba8c3ca1d1
Luca Mandolesi <pyarchinit@gmail.com>
2016-07-14 15:31:27 +0200

    Modifica valore di loop dell'ordine dei layer
    modificata la linea 1747
    self.order_count >= 50:
    
    modificando tale valore numerico, il loop per creazione dell'ordine dei
    layer cambia di durata. Sarebbe auspicabile inserire una casella
    modificabile dentro alla scheda US.

--- pyarchinit/modules/gis/pyarchinit_pyqgis.py
+++ pyarchinit/modules/gis/pyarchinit_pyqgis.py
@@ -1744,7 +1744,7 @@ class Order_layer_v2:
 			if bool(matrix_us_level) == False:
 				test = 1
 				return self.order_dict
-			elif self.order_count >= 10000:
+			elif self.order_count >= 50:
 				test = 1
 				QMessageBox.warning(self, "Errore",str(self.order_count), QMessageBox.Ok)
 
